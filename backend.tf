3732861be4ae1a45bfc6eeec3f546b9edd9001d7
HireReady381 <bobthebuilder64657@gmail.com>
2026-04-02 18:18:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "pathnex-feb-2026-batch"
-    key            = "pathnex/monitoring/terraform.tfstate"
+    bucket         = "HireReady-feb-2026-batch"
+    key            = "HireReady/monitoring/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
